a9ac66f510d77183874a3585aedad600e579d65c
clintonictendency@gmail.com <clintonictendency@gmail.com>
2026-04-29 18:34:05 +0100

    updated github.controller file with the correct redirect url

--- controllers/github.controller.js
+++ controllers/github.controller.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { generateAccessToken, 
+const { generateAccessToken,
   generateRefreshToken } = require("../services/RefreshToken.service");
 const axios = require("axios");
 const User = require("../models/User.model");
@@ -31,27 +31,28 @@ const { generateCodeVerifier, generateCodeChallenge } = require("../utils/Pkce.u
 exports.getGitHub = (req, res) => {
   try {
     const verifier = generateCodeVerifier();
-  const challenge = generateCodeChallenge(verifier);
+    const challenge = generateCodeChallenge(verifier);
 
-  if (!req.session) {
-  throw new Error("Session middleware not configured");
-}
-  // store verifier temporarily (Redis or memory for now)
-  req.session.code_verifier = verifier;
+    if (!req.session) {
+      throw new Error("Session middleware not configured");
+    }
+    // store verifier temporarily (Redis or memory for now)
+    req.session.code_verifier = verifier;
 
-  const params = new URLSearchParams({
-  client_id: process.env.GITHUB_CLIENT_ID,
-  code_challenge: challenge,
-  code_challenge_method: "S256"
-});
+    const params = new URLSearchParams({
+      client_id: process.env.GITHUB_CLIENT_ID,
+      code_challenge: challenge,
+      code_challenge_method: "S256"
+    });
 
-const githubUrl = `https://github.com/login/oauth/authorize?${params.toString()}`;
+    const githubUrl = `https://github.com/login/oauth/authorize?${params.toString()}`;
 
-  // const githubUrl = `https://github.com/login/oauth/authorize
-  // ?client_id=${process.env.GITHUB_CLIENT_ID}
-  // &code_challenge=${challenge}&code_challenge_method=S256`;
+    // const githubUrl = `https://github.com/login/oauth/authorize
+    // ?client_id=${process.env.GITHUB_CLIENT_ID}
+    // &code_challenge=${challenge}&code_challenge_method=S256`;
 
-  res.redirect(githubUrl);
+    res.redirect(githubUrl);
+
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Failed to redirect to GitHub" });
@@ -62,8 +63,8 @@ const githubUrl = `https://github.com/login/oauth/authorize?${params.toString()}
 
 
 exports.gitHubCallBack = async (req, res) => {
- try {
-  //  const { code } = req.query;
+  try {
+    //  const { code } = req.query;
     const code = req.query.code;
 
     if (!code) {
@@ -74,7 +75,7 @@ exports.gitHubCallBack = async (req, res) => {
       return res.status(400).json({ error: "Missing code_verifier" });
     }
 
-  const tokenRes = await axios.post(
+    const tokenRes = await axios.post(
       "https://github.com/login/oauth/access_token",
       {
         client_id: process.env.GITHUB_CLIENT_ID,
@@ -83,48 +84,49 @@ exports.gitHubCallBack = async (req, res) => {
         code_verifier: req.session.code_verifier
       },
       { headers: { Accept: "application/json" } }
-  );
-console.log("VERIFIER:", req.session.code_verifier);
+    );
+    console.log("VERIFIER:", req.session.code_verifier);
     const accessToken = tokenRes.data.access_token;
 
-  const userRes = await axios.get("https://api.github.com/user", {
-    headers: { Authorization: `Bearer ${accessToken}` }
-  });
-
-  // const emailRes = await axios.get('https://api.github.com/user/emails', {
-	// 	headers: { Authorization: `Bearer ${accessToken}` },
-	// });
-
-	// const email = emailRes.data.find((e) => e.primary && e.verified)?.email;
-
-  const githubUser = userRes.data;
-
-  let user = await User.findOne({ githubId: githubUser.id });
-
-  if (!user) {
-    user = await User.create({
-      githubId: githubUser.id,
-      username: githubUser.login,
-      avatar_url: githubUser.avatar_url,
-      provider: "github"
+    const userRes = await axios.get("https://api.github.com/user", {
+      headers: { Authorization: `Bearer ${accessToken}` }
     });
+
+    // const emailRes = await axios.get('https://api.github.com/user/emails', {
+    // 	headers: { Authorization: `Bearer ${accessToken}` },
+    // });
+
+    // const email = emailRes.data.find((e) => e.primary && e.verified)?.email;
+
+    const githubUser = userRes.data;
+
+    let user = await User.findOne({ githubId: githubUser.id });
+
+    if (!user) {
+      user = await User.create({
+        githubId: githubUser.id,
+        username: githubUser.login,
+        avatar_url: githubUser.avatar_url,
+        provider: "github"
+      });
+    }
+    console.log("Authenticated user:", user);
+
+    // Issue tokens
+    const jwtAccess = generateAccessToken(user);
+    const refresh = await generateRefreshToken(user);
+
+    // res.json({ access_token: jwtAccess, refresh_token: refresh });
+    res.redirect(`${process.env.WEB_PORTAL_URL}dashboard`)
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: "Failed to handle GitHub callback",
+      error_details: error.response?.data || error.message
+    });
+
   }
-  console.log("Authenticated user:", user);
-
-  // Issue tokens
-  const jwtAccess = generateAccessToken(user);
-  const refresh = await generateRefreshToken(user);
-
-  res.json({ access_token: jwtAccess, refresh_token: refresh });
-
- } catch (error) {
-  console.error(error);
-  res.status(500).json({ 
-    error: "Failed to handle GitHub callback" ,
-    error_details: error.response?.data || error.message
-  });
-
- }
 };
 
 // module.exports = getGitHub;
